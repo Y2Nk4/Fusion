5f18cc6098af0a588a8f8d35a57d14bbd65791fa
Y2Nk4 <1147678303@qq.com>
2020-10-25 01:13:55 -0400

    update

--- socket.js
+++ socket.js
@@ -16,58 +16,58 @@ class SocketHandler extends EventEmitter {
         this.isConnectedOrigin = false
 
         if (options.sendUDP) {
-            // UDP Protocol
-            this.originSocket = udpProto.connect()
+            // send through UDP Protocol
+            this.originSocket = udpProto.connect(options.mainServerPort, options.mainServerHost)
         } else {
             // Using TCP
             this.originSocket = new net.Socket();
-
-            this.originSocket.on('error', (error) => {
-                console.log('error', error)
-            })
-            this.socket.on('error', (error) => {
-                console.log('socket error', error)
-            })
-
-            /*this.originSocket.connect(options.mainServerPort, options.mainServerHost, () => {
-                console.log('connected to origin')
-
-                this.originSocket.pipe(this.socket)
-                this.socket.pipe(this.originSocket)
-            })
-
-            this.socket.on('data', (clientData) => {
-                console.log('receiving data:', clientData)
-            })*/
-
-            this.originSocket.connect(options.mainServerPort, options.mainServerHost, () => {
-                console.log('connected to main server')
-            })
-
-            this.socket.on('data', (clientData) => {
-                console.log(clientData)
-
-                this.originSocket.write(clientData)
-            })
-            this.originSocket.on('data', (serverData) => {
-                console.log('origin return', this.live, serverData)
-                if (this.live){
-                    this.socket.write(serverData)
-                }
-            })
-
-            this.socket.on('close', () => {
-                this.live = false
-                console.log('client socket closed')
-                this.emit('close')
-
-                this.originSocket.destroy()
-            })
-
-            this.socket.on('end', () => {
-                console.log('client socket ends')
-            })
         }
+
+        this.originSocket.on('error', (error) => {
+            console.log('error', error)
+        })
+        this.socket.on('error', (error) => {
+            console.log('socket error', error)
+        })
+
+        /*this.originSocket.connect(options.mainServerPort, options.mainServerHost, () => {
+            console.log('connected to origin')
+
+            this.originSocket.pipe(this.socket)
+            this.socket.pipe(this.originSocket)
+        })
+
+        this.socket.on('data', (clientData) => {
+            console.log('receiving data:', clientData)
+        })*/
+
+        this.originSocket.connect(options.mainServerPort, options.mainServerHost, () => {
+            console.log('connected to main server')
+        })
+
+        this.socket.on('data', (clientData) => {
+            console.log(clientData)
+
+            this.originSocket.write(clientData)
+        })
+        this.originSocket.on('data', (serverData) => {
+            console.log('origin return', this.live, serverData)
+            if (this.live){
+                this.socket.write(serverData)
+            }
+        })
+
+        this.socket.on('close', () => {
+            this.live = false
+            console.log('client socket closed')
+            this.emit('close')
+
+            if (this.originSocket.destroy) this.originSocket.destroy()
+        })
+
+        this.socket.on('end', () => {
+            console.log('client socket ends')
+        })
     }
 }
 
